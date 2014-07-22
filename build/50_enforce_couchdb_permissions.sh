603148ad9dac67c710f1b65bb30528d6e05259a7
Terin Stock <terinjokes@gmail.com>
2014-07-22 01:13:22 -0700

    Seems this is extremely important.

--- build/50_enforce_couchdb_permissions.sh
+++ build/50_enforce_couchdb_permissions.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Docker attaches shared directories as owned by root
 set -e
 chown -R couchdb:couchdb /opt/couchdb/etc/couchdb /opt/couchdb/var/lib/couchdb /opt/couchdb/var/log/couchdb /opt/couchdb/var/run/couchdb
